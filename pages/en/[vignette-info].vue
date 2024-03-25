0ba111ce1c8552cb958967f59a71e257117db709
zsena <tamas.zsanett@voxinfo.hu>
2024-03-25 12:54:59 +0100

    pages testing

---
+++ pages/en/[vignette-info].vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="">
+    <section class="min-h-[700px] container mx-auto px-4 xl:my-28">
+      <h1 class="h1">
+        vignette info test page
+      </h1>
+      <div class="flex flex-col lg:flex-row gap-8 xl:gap-40 my-16 xl:my-28">
+       
+      </div>
+    </section>
+  </main>
+</template>
