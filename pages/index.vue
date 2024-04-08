48199762d507d5b8f2eb10f1504832cf4d8ae9c1
zsena <tamas.zsanett@voxinfo.hu>
2024-04-08 16:15:34 +0200

    index page changes

--- pages/index.vue
+++ pages/index.vue
@@ -2,36 +2,19 @@
   <!-- bg-red-500 bg-blue-500 bg-teal-500 -->
   <main class="">
     <section class="min-h-[700px] container mx-auto px-4 pt-6">
-      <div class="flex flex-col gap-8 xl:gap-20 my-16 xl:my-28">
-        <WidgetVignetteAccordion />
-      </div>
-      <div class="flex flex-col gap-8 xl:gap-20 my-16 xl:my-28">
-        <WidgetPurchaseProcess />
-      </div>
-      <div class="flex flex-col gap-8 xl:gap-20 my-16 xl:my-28">
-        <WidgetTypeOfBaseTime />
-      </div>
-      <div class="flex flex-col gap-8 xl:gap-20 my-16 xl:my-28">
-        <WidgetFaq />
-      </div>
-      <section class="my-16 xl:my-28">
-        <h2>Recent news</h2>
-        <div
-          class="grid sm:grid-cols-2 md:grid-cols-3 content-center gap-8 xl:gap-10"
-        >
-          <div v-for="item in newsWidget.content" :key="item.ID">
-            <WidgetNews :contentItem="item" :siteUrl="siteUrl" />
-          </div>
-        </div>
-      </section>
+      <WidgetMainTextBox />
+      <WidgetVignetteAccordion />
+      <WidgetPurchaseProcess />
+      <WidgetTypeOfBaseTime />
+      <WidgetTypeCategory />
+      <WidgetWhyYouBuyHere />
+      <WidgetFaq />
+      <WidgetNews />
     </section>
   </main>
 </template>
 <script setup>
 import { ref } from "vue";
-import newsWidgetData from "@/static/newsWidget.json";
-const newsWidget = ref(newsWidgetData);
-const siteUrl = ref(newsWidget.value.siteUrl); //
 
 const visible = ref(false);
 </script>
