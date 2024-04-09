b7ade06fe3a487ad5ec474f922fe4215379a9bc9
zsena <tamas.zsanett@voxinfo.hu>
2024-04-09 16:43:10 +0200

    css fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
-  <!-- bg-red-500 bg-blue-500 bg-teal-500 -->
   <main class="">
-    <section class="min-h-[700px] container mx-auto px-4 lg:px-0 pt-6">
+    <section class="container mx-auto px-4 lg:px-0 pt-6">
       <WidgetMainTextBox />
       <WidgetVignetteAccordion />
       <WidgetPurchaseProcess />
