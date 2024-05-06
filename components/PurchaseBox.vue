c84dd6416996f62276697286c1a7e1505910543a
zsena <tamas.zsanett@voxinfo.hu>
2024-05-06 19:26:28 +0200

    text fix

--- components/PurchaseBox.vue
+++ components/PurchaseBox.vue
@@ -1,22 +1,22 @@
 <template>
   <section class="shadow bg-green text-white purchase-calculator">
     <div class="w-full flex items-center justify-center py-4">
-      <span>Kiválasztott matricák </span>
+      <span>Selected vignettes</span>
     </div>
     <div
       class="bg-white text-base-black font-bold w-full flex items-center justify-between"
     >
-      <div class="w-1/2 py-3">D1 - 10 napos</div>
-      <div class="w-1/2">6 400 Ft</div>
+      <div class="w-1/2 py-3">D1 - 1 month</div>
+      <div class="w-1/2">10 360 Ft</div>
     </div>
     <div
       class="bg-white text-base-black font-bold w-full flex items-center justify-between"
     >
-      <div class="w-1/2 py-3">D1 - 10 napos</div>
-      <div class="w-1/2 py-3">6 400 Ft</div>
+      <div class="w-1/2 py-3">D1 - 1 month</div>
+      <div class="w-1/2 py-3">10 360 Ft</div>
     </div>
     <div class="w-full flex items-center justify-between py-1">
-      <div class="w-1/2 py-3">Összesen:</div>
+      <div class="w-1/2 py-3">Total:</div>
       <div class="w-1/2 py-3">12 800 Ft</div>
     </div>
   </section>
