f4f7cc1cc7d6713ca00a47a4b52248505e9b49b9
zsena <tamas.zsanett@voxinfo.hu>
2024-05-31 16:35:40 +0200

    purchase calc ux fix

--- components/PurchaseCalculator.vue
+++ components/PurchaseCalculator.vue
@@ -3,15 +3,17 @@
     <div class="w-full flex items-center justify-center py-4">
       <span>{{ title }}</span>
     </div>
-    <template v-for="(item, index) in list" :key="index">
-      <div class="bg-white text-base-black font-bold w-full flex items-center justify-between">
-        <div class="w-1/2 p-3 text-left">{{ item.category }} - {{ item.durationType }}</div>
-        <div :class="['w-1/2 px-3 text-right', { 'py-3': index % 2 === 1 }]">{{ item.price }}</div>
-      </div>
-    </template>
+    <div class="max-h-[250px] overflow-y-auto">
+      <template v-for="(item, index) in list" :key="index">
+        <div class="bg-white text-base-black font-bold w-full flex items-center justify-between">
+          <div class="w-2/3 p-3 text-left">{{ item.category }} - {{ item.durationType }}</div>
+          <div :class="['w-1/3 pr-3 text-right', { 'py-3': index % 2 === 1 }]">{{ item.price }}</div>
+        </div>
+      </template>
+    </div>
     <div class="w-full flex items-center justify-between py-1">
-      <div class="w-1/2 p-3 text-left">Total:</div>
-      <div class="w-1/2 p-3 text-right">{{ totalPrice }}</div>
+      <div class="w-2/6 pl-3 py-3 text-left">Total:</div>
+      <div class="w-3/6 pr-6 text-right">{{ totalPrice }}</div>
     </div>
   </section>
 </template>
